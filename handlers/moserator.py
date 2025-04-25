2642af0494625d6d237728786e185ddd76a5405e
NoName <foobar@example.com>
2025-04-25 19:47:00 +0300

    додоелать функцию с сообщением размьюта сделать срабатывание на запрет слова в голосовых сообщениях

--- handlers/moserator.py
+++ handlers/moserator.py
@@ -1,10 +1,16 @@
 import sqlite3
+from aiogram import Bot
 from aiogram.types import Message
 from loader import (router, FORBIDEN_WORDS, user_violations, MAX_VIOLATIONS, MUTE_DURATION)
 from datetime import timedelta, datetime
 con = sqlite3.connect("data/data.db", check_same_thread=False)
 cursor = con.cursor()
 message = Message
+
+async def send_end_mute(user_id,bot:Bot):
+    await bot.send_message(text='вы снова можете писать в чат', chat_id=user_id)
+
+
 async def record_violations(user_id):
     if user_id not in user_violations:
         user_violations[user_id] = {
@@ -21,9 +27,9 @@ async def record_violations(user_id):
 async def check_user_mut(user_id):
     if user_id in user_violations:
         violations = user_violations[user_id]
-        if violations['count'] % MAX_VIOLATIONS == 0:
+        if violations['count'] % MAX_VIOLATIONS == 1:
             mute_end = violations[('last_violations'
-                                   '')] + timedelta(minutes=(1 + 5 * violations['count_viol']))
+                                   '')] + timedelta(minutes=(1 + 1 * violations['count_viol']))
 
             if datetime.now() < mute_end:
                 return True
@@ -32,11 +38,13 @@ async def check_user_mut(user_id):
                 violations['count_viol'] += 1
                 violations['count'] = violations['count']
                 violations['last_violations'] = None
+                message.send_end_mute()
 
-                print(violations)
+
+
             cursor.execute("INSERT INTO viols (count_violations, count, id) VALUES (?,?,?)",(violations['count_viol'], violations['count'], user_id))
             con.commit()
-            if violations['count']> 10:
+            if violations['count']> 4:
                 await message.bot.ban_chat_member()
             else:
                 return False
@@ -48,14 +56,19 @@ async def check_user_mut(user_id):
 
 @router.message()
 async def handle_message(message: Message):
-    user_id = message.from_user.id
 
+
+    user_id = message.from_user.id
+    violations = user_violations[user_id]
     if await check_user_mut(user_id):
-        violations = user_violations[user_id]
+
         await message.delete()
         await message.answer(
             f'@{message.from_user.username}, вы временно ограничены'f'в отправке сообщений на {1 + 5 * violations["count_viol"]} минут из-за частых нарушений.')
         return
+
+
+
 
     if message.entities:
         for entity in message.entities:
