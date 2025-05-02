783052793bee974f8422d534a7f7562da2651c64
NoName <foobar@example.com>
2025-05-02 19:47:08 +0300

    додоелать функцию с сообщением размьюта сделать срабатывание на запрет слова в голосовых сообщениях

--- handlers/moserator.py
+++ handlers/moserator.py
@@ -1,14 +1,15 @@
 import sqlite3
 from aiogram import Bot
 from aiogram.types import Message
-from loader import (router, FORBIDEN_WORDS, user_violations, MAX_VIOLATIONS, MUTE_DURATION)
+from loader import (router, FORBIDEN_WORDS, user_violations, MAX_VIOLATIONS, MUTE_DURATION, MIN_VIOLATIONS, bot)
 from datetime import timedelta, datetime
 con = sqlite3.connect("data/data.db", check_same_thread=False)
 cursor = con.cursor()
 message = Message
-
-async def send_end_mute(user_id,bot:Bot):
+async def send_end_mute(user_id,bot):
     await bot.send_message(text='вы снова можете писать в чат', chat_id=user_id)
+async def ban_member(user_id,bot):
+    await bot.ban_chat_member(chat_id='@+0HGdE2soYw5iMzky',user_id=user_id)
 
 
 async def record_violations(user_id):
@@ -27,29 +28,24 @@ async def record_violations(user_id):
 async def check_user_mut(user_id):
     if user_id in user_violations:
         violations = user_violations[user_id]
-        if violations['count'] % MAX_VIOLATIONS == 1:
-            mute_end = violations[('last_violations'
-                                   '')] + timedelta(minutes=(1 + 1 * violations['count_viol']))
+        if violations['count'] >= MIN_VIOLATIONS:
+            mute_end = violations['last_violations'] + timedelta(seconds=(1 + 1 * violations['count_viol']))
 
             if datetime.now() < mute_end:
                 return True
+
             else:
 
                 violations['count_viol'] += 1
                 violations['count'] = violations['count']
                 violations['last_violations'] = None
-                message.send_end_mute()
-
-
-
-            cursor.execute("INSERT INTO viols (count_violations, count, id) VALUES (?,?,?)",(violations['count_viol'], violations['count'], user_id))
-            con.commit()
-            if violations['count']> 4:
-                await message.bot.ban_chat_member()
-            else:
-                return False
-
-
+                cursor.execute("INSERT INTO  viols (count_violations, count, id) VALUES (?,?,?)",
+                               (violations['count_viol'], violations['count'], user_id))
+                con.commit()
+                if violations['count'] >= MAX_VIOLATIONS:
+                    return await ban_member(user_id, bot)
+                else:
+                    return False
 
         return False
 
@@ -57,18 +53,15 @@ async def check_user_mut(user_id):
 @router.message()
 async def handle_message(message: Message):
 
-
     user_id = message.from_user.id
-    violations = user_violations[user_id]
-    if await check_user_mut(user_id):
 
+
+    if await check_user_mut(user_id):
+        violations = user_violations[user_id]
         await message.delete()
         await message.answer(
             f'@{message.from_user.username}, вы временно ограничены'f'в отправке сообщений на {1 + 5 * violations["count_viol"]} минут из-за частых нарушений.')
         return
-
-
-
 
     if message.entities:
         for entity in message.entities:
@@ -85,4 +78,4 @@ async def handle_message(message: Message):
             await message.delete()
             await record_violations(user_id)
             await message.answer(
-                f'@{message.from_user.username}, сообщение удалено:'f"содержит запреьное слово. Нарушение#{user_violations[user_id]['count']}")
+                f'@{message.from_user.username}, сообщение удалено:'f"содержит запрещённое слово. Нарушение#{user_violations[user_id]['count']}")
